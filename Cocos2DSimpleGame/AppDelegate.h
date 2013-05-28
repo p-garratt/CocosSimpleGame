eec9a54dfc92c5364d850d45aecb641c777c50b8
p-garratt <pgarratt@hmc.edu>
2013-05-28 14:57:23 -0700

    Initial Commit

---
+++ Cocos2DSimpleGame/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  Cocos2DSimpleGame
+//
+//  Created by Paige Garratt on 5/28/13.
+//  Copyright Paige Garratt 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Cocos2DSimpleGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2DSimpleGame
+//
+//  Created by Paige Garratt on 5/28/13.
+//  Copyright Paige Garratt 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
